4d35924a2ff2c01f1dde63eee974e68bfb1c9f90
Ricardo Tonietti <rictonietti@gmail.com>
2023-02-10 20:09:03 -0300

    add methods GET ALL e GET ONE

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -4,12 +4,17 @@ import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.EmailModel;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EmailService {
@@ -19,6 +24,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Transactional
     public EmailModel sendEmail(EmailModel emailModel) {
         emailModel.setSendDateEmail(LocalDateTime.now());
 
@@ -37,6 +43,13 @@ public class EmailService {
             return emailRepository.save(emailModel);
 
         }
+    }
 
+    public Page<EmailModel> findAll(Pageable pageable){
+        return emailRepository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(UUID emalId){
+        return emailRepository.findById(emalId);
     }
 }
